d399666ed81c48bd8fc31ed78af2a4583bbaeeaf
bitranox <1uGToNSiYo85pWhQcqFu>
2019-07-22 19:09:46 +0200

    refractor

--- build_docs.py
+++ build_docs.py
@@ -16,7 +16,7 @@ else:
     from rst_include.libs import lib_log
 
 # CONSTANTS & PROJECT SPECIFIC FUNCTIONS
-codeclimate_link_hash = "64c43749ac6b4c52478d"
+codeclimate_link_hash = "64c43749ac6b4c52478d"  # for lib_list
 
 
 def project_specific(repository_slug, repository, repository_dashed):
